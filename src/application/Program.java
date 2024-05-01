fd1154277db0f55395917c84a8d196fc60b3d79e
Tássia Pereira <tassiapereira2020@gmail.com>
2024-05-01 11:02:25 -0300

    reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -15,6 +15,8 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.creatSellerDao();
 		
+		System.out.println("=== test 1: seller findById ====");
+		
 		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
