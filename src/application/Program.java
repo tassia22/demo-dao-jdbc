39a8ad3721f2b1ebd725158d19e579aa375ea85b
Tássia Pereira <tassiapereira2020@gmail.com>
2024-05-01 11:47:20 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,11 +17,19 @@ public class Program {
 		
 		System.out.println("=== test 1: seller findById ====");
 		
+		Seller seller = sellerDao.findById(3);
+		System.out.println(seller);
 		
 		System.out.println("\n=== test 2: seller findByDepartment ====");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		for(Seller obj: list) {
+			System.out.println(obj);
+		}
+		
+		System.out.println("\n=== test 3: seller findAll ====");
+	    list = sellerDao.findAll();
+	    for(Seller obj: list) {
 			System.out.println(obj);
 		}
 
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -102,15 +102,53 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		//buscar todos os vendedores do departamento e ordenar por nome
+		
+		try {
+			st = conn.prepareStatement(
+					"select seller.*,department.Name as DepName"
+					+"from seller inner join department"
+					+"on seller.DepartmentId = department.Id"
+					+"order by Name");
+			
+			
+			rs = st.executeQuery();
+			
+			List<Seller> list = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap();
+			
+			while (rs.next()) {
+				
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				
+				if(dep == null) {
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+				
+				Seller obj = instatiateSeller(rs, dep);
+				list.add(obj);
+				
+			}
+			return list;
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 	@Override
 	public List<Seller> findByDepartment(Department department){
 		PreparedStatement st = null;
 		ResultSet rs = null;
 		
-		//retornar um vendedor por Id
+		//retornar um vendedor por departamento
 		
 		try {
 			st = conn.prepareStatement(
